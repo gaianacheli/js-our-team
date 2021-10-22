d050ca24004e60097e99ea577deab436537fc48e
gaianacheli <gaianacheli04@gmail.com>
2021-10-22 14:41:16 +0200

    esrcizio completo

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const Team = [
+{
+    name: 'Ivan',
+    role: 'dottore',
+    img: 'https://picsum.photos/id/1025/300/300'
+},
+{
+    name: 'Anna',
+    role: 'infermiera',
+    img: 'https://picsum.photos/id/1025/300/300'
+},
+{
+    name: 'Chiara',
+    role: 'chirurgo',
+    img: 'https://picsum.photos/id/1025/300/300'
+}
+]
+console.log(Team);
